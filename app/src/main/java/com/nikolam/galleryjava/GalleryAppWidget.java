60b8a2fd937e5799085660aacad69f8bf9895a0d
Nikola M <nikolamilovic2001@gmail.com>
2020-10-03 11:33:18 +0200

    added widget

---
+++ app/src/main/java/com/nikolam/galleryjava/GalleryAppWidget.java
@@ -0,0 +1,47 @@
+package com.nikolam.galleryjava;
+
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.widget.RemoteViews;
+
+import androidx.annotation.NonNull;
+
+/**
+ * Implementation of App Widget functionality.
+ */
+public class GalleryAppWidget extends AppWidgetProvider {
+
+    static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
+                                int appWidgetId) {
+
+        // Construct the RemoteViews object
+        RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.gallery_app_widget);
+      //  views.setImageViewUri(R.id.widget_imageView);
+        views.setImageViewUri(R.id.widget_imageView, Uri.EMPTY);
+
+        // Instruct the widget manager to update the widget
+        appWidgetManager.updateAppWidget(appWidgetId, views);
+    }
+
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        // There may be multiple widgets active, so update all of them
+        for (int appWidgetId : appWidgetIds) {
+            updateAppWidget(context, appWidgetManager, appWidgetId);
+        }
+    }
+
+    @Override
+    public void onEnabled(Context context) {
+        // Enter relevant functionality for when the first widget is created
+    }
+
+    @Override
+    public void onDisabled(Context context) {
+        // Enter relevant functionality for when the last widget is disabled
+    }
+}
